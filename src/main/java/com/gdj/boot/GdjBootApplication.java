4770b75eadb332a4534d9cb4c946b8a0fbab1e41
SeokHoon <shheo90oi@gmail.com>
2024-03-05 12:38:43 +0900

    Project init

---
+++ src/main/java/com/gdj/boot/GdjBootApplication.java
@@ -0,0 +1,13 @@
+package com.gdj.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdjBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdjBootApplication.class, args);
+	}
+
+}
