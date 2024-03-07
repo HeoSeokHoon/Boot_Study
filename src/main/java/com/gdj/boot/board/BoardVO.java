a1e8c5489e40a3a14ef0b6dd4749c1c5eb95ad40
SeokHoon <shheo90oi@gmail.com>
2024-03-07 16:43:38 +0900

    AOP Transaction

--- src/main/java/com/gdj/boot/board/BoardVO.java
+++ src/main/java/com/gdj/boot/board/BoardVO.java
@@ -3,10 +3,7 @@ package com.gdj.boot.board;
 import java.sql.Date;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 //@Data
